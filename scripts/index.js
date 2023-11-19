46c0f53ab3ac0044926eb33b231b9559ada7e348
Akseenja <klasik1995@bk.ru>
2023-11-19 15:36:08 +0300

    first code

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const placesContainer = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+function addCard(card, remove) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = card.link;
+  cardElement.querySelector(".card__title").textContent = card.name;
 
-// @todo: Функция создания карточки
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", (event) => remove(event));
 
-// @todo: Функция удаления карточки
+  return cardElement;
+}
 
-// @todo: Вывести карточки на страницу
+function deleteCard(event) {
+  const card = event.target.closest(".card");
+  card.remove();
+}
+
+initialCards.forEach((item) => {
+  placesContainer.append(addCard(item, deleteCard));
+});
